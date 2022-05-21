f42f059c7d2e08a088ca4a3cde44c30d023a9862
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2022-05-21 16:47:36 -0500

    Concurrency tested.

--- DriverSensores/src/main/java/driver/DriverSensores.java
+++ DriverSensores/src/main/java/driver/DriverSensores.java
@@ -7,5 +7,6 @@ public class DriverSensores {
     public static void main(String[] args) throws Exception {
         // TODO: Averiguar cómo se debe hacer el manejo de hilos (cada sensor debe correr en paralelo a los demás).
         Sensor sensor1 = new Sensor(TipoSensor.PH, 2000, "/Users/cristobalcastrilonbalcazar/Dev/SistemaCalidadAgua/DriverSensores/pH_895e81e9-0cbe-4e96-aa84-bf86a8ee07f1_configFile.csv");
+        Sensor sensor2 = new Sensor(TipoSensor.PH, 1000, "");
     }
 }
